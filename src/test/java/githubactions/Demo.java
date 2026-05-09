866531dbbc5229487244c8d8fced8de32ff45c16
Prasad Withanage <aero.kwps@live.co.uk>
2026-05-09 11:30:01 +0530

    Allure reports added

--- src/test/java/githubactions/Demo.java
+++ src/test/java/githubactions/Demo.java
@@ -1,6 +1,12 @@
 package githubactions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,11 +18,14 @@ import org.testng.annotations.Test;
  * @author aerok
  * @code @created 09/05/2026
  */
+@Epic("Web UI Automation")
+@Feature("Search Engine and Q&A Tests")
 public class Demo {
 
     protected WebDriver webDriver;
 
     @BeforeMethod
+    @Step("Setting up Chrome WebDriver")
     public void setup(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
@@ -27,24 +36,38 @@ public class Demo {
         webDriver = new ChromeDriver(options);
     }
 
-    @Test
+    @Test(description = "Verify Google Homepage")
+    @Description("This test navigates to Google and prints the page title.")
+    @Severity(SeverityLevel.NORMAL)
     public void testCase1(){
-        webDriver.get("https://www.google.com");
-        System.out.println("Title "+webDriver.getTitle());
+        navigateToUrl("https://www.google.com");
+        printPageTitle();
     }
 
-    @Test
+    @Test(description = "Verify StackOverflow Homepage")
+    @Description("This test navigates to StackOverflow and prints the page title.")
+    @Severity(SeverityLevel.NORMAL)
     public void testCase2(){
-        webDriver.get("https://www.stackoverflow.com");
-        System.out.println(webDriver.getTitle());
+        navigateToUrl("https://www.stackoverflow.com");
+        printPageTitle();
+    }
+
+    @Step("Navigating to URL: {0}")
+    public void navigateToUrl(String url) {
+        webDriver.get(url);
+    }
+
+    @Step("Printing the page title")
+    public void printPageTitle() {
+        System.out.println("Title " + webDriver.getTitle());
     }
 
     @AfterMethod
+    @Step("Tearing down WebDriver")
     public void tearDown(){
         if (webDriver != null){
             webDriver.quit();
         }
-
     }
 
 }
